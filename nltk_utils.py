be4a343a63d8e12d0575eefe8b44b38428194c59
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-09-06 17:04:06 +0700

    Implement bag of word

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,3 +1,5 @@
+import enum
+import numpy as np
 import nltk
 from nltk.stem.porter import PorterStemmer
 
@@ -10,4 +12,11 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
